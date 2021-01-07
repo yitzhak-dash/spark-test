d915adf74c0f27ee90f5a31b15f311b1a59d08cb
Yitzhak Dashevsky <yitzhakd@compiralabs.com>
2021-01-07 18:11:03 +0200

    order files in the dir

---
+++ rdd/total-spen-by-customer.py
@@ -0,0 +1,20 @@
+from pyspark import SparkConf, SparkContext
+
+conf = SparkConf().setMaster('local').setAppName('RatingHistogram')
+sc = SparkContext(conf=conf)
+
+
+def parse_line(ln):
+    fields = ln.split(',')
+    customer_id = int(fields[0])
+    amount = float(fields[2])
+    return customer_id, amount
+
+
+lines = sc.textFile('./data/myFile0.csv')
+parsed_lines = lines.map(parse_line)
+customers = parsed_lines.reduceByKey(lambda x, y: x + y).map(lambda x_y: (x_y[1], x_y[0])).sortByKey()
+results = customers.collect()
+
+for res in results:
+    print('customer: {1}\t\t({0}$)'.format(res[0], res[1]))
